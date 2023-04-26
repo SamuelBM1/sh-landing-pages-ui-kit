2350a2b9a64d43293328879c378e1e87cd781d31
SamuelBM1 <sbarragan@sales-hub.com>
2023-04-26 00:07:46 -0700

    Most of them finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { GetStartedComponent } from './pages/get-started/get-started.component';
 import { ButtonsComponent } from './pages/buttons/buttons.component';
 import { LoaderComponent } from './pages/loader/loader.component';
 import { IconsComponent } from './pages/icons/icons.component';
+import { InputsComponent } from './pages/inputs/inputs.component';
+import { DialogsComponent } from './pages/dialogs/dialogs.component';
 
 const routes: Routes = [
   { path: "", component: GetStartedComponent },
   { path: "buttons", component: ButtonsComponent },
   { path: "loader", component: LoaderComponent },
   { path: "icons", component: IconsComponent },
+  { path: "inputs", component: InputsComponent },
+  { path: "dialogs", component: DialogsComponent },
   { path: '**', redirectTo: "" }
 ];
 
--- src/app/pages/inputs/inputs.component.ts
+++ src/app/pages/inputs/inputs.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class InputsComponent {
 
+  buttons: {type: "basic" | "outlined" | "cta" | "cta-outlined" | "3D", label: string}[] = [
+    {
+      type: "basic",
+      label: "Basic"
+    },
+    {
+      type: "cta",
+      label: "CTYA"
+    },
+    {
+      type: "outlined",
+      label: "Outlined"
+    },
+    {
+      type: "cta-outlined",
+      label: "CTA Outlined"
+    },
+    {
+      type: "3D",
+      label: "3D"
+    },
+  ]
+
 }
